610822ed14958896c6065389dcdd38e66f6908c1
Sparky <17860180+sparkins@users.noreply.github.com>
2018-10-31 18:22:10 -0700

    added integration  to firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.5.6/firebase.js"></script>
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAPBjfbWjYgZsJ00tgPQPkRmjNpy6I3qpQ",
+    authDomain: "team-iceberg.firebaseapp.com",
+    databaseURL: "https://team-iceberg.firebaseio.com",
+    projectId: "team-iceberg",
+    storageBucket: "team-iceberg.appspot.com",
+    messagingSenderId: "997146402050"
+  };
+  firebase.initializeApp(config);
